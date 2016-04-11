b681f59bd280c3bd6f98b01b977e0caa274e447f
Chris James <chris@chris-james.me>
2016-04-11 08:27:54 -0400

    Initial commit

---
+++ client/newHackerForm/newHackerForm.js
@@ -0,0 +1,21 @@
+Template.newHackerForm.events({
+  'submit #hackerForm' : function(event, template) {
+
+    event.preventDefault();
+
+    const hackerName = template.find('#hackerName').value;
+    const legalName = template.find('#legalName').value;
+    const email = template.find('#email').value;
+    const allegiance = template.find('#allegiance').value;
+
+    const newHacker = {
+      hackerName: hackerName,
+      legalName: legalName,
+      email: email,
+      allegiance: allegiance
+    }
+
+    console.log(newHacker);
+  }
+
+});
